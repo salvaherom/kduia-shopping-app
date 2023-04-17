d6271883ce3ca7e1f1881d151418ecca6598f7f6
Brian Salvador <brian.herom@proton.me>
2023-04-17 00:18:13 -0400

    feat(items): item list components are created

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,27 @@ import { AppContext } from '../context/AppContext';
 import { FaTimesCircle } from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
+    const { id, name, unitprice, quantity } = props;
+    const handleDeleteItem = () => {
+        const item = {
+            name
+        };
+        
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload: item,
+        });
+    };
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{quantity}</td>
+            <td>{Location}{parseInt(unitPrice)}</td>
+            <td>{Location}{parseInt(quantity) * parseInt(unitPrice)}</td>
+            <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteItem}></FaTimesCircle></td>
+        </tr>
+    );
 };
 
 export default ExpenseItem;
